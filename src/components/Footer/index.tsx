00ed1d41478fd94142f3faff7ce01cbb9c1504d3
Hasan Atahan <hasan3079.hc@gmail.com>
2024-02-29 03:09:31 +0300

    links-added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,27 @@
-import './styles/footer.css'
+import "./styles/footer.css";
 
 const Footer = () => {
   return (
-    <footer className='footer-container'>
-        <div className='footer-context'>
-            <label>Hasan Atahan</label>
-            <div className='social'>
-                <a href='#' >Linkedin</a>
-                <a href='#' >Github Repo</a>
-            </div>
+    <footer className="footer-container">
+      <div className="footer-context">
+        <label>Hasan Atahan</label>
+        <div className="social">
+          <a
+            href="https://www.linkedin.com/in/hasan-atahan-a655ba10b/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://github.com/atahandevelopment/secil-case"
+            target="_blank"
+          >
+            Github Repo
+          </a>
         </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
